dd1fc86695477288aacc0cb9a7561c14ad884fa7
Yogini Aishwaryaa P T S <yoginianithakumar@gmail.com>
2026-03-07 14:35:22 +0530

    add package.json

--- backend/server.js
+++ backend/server.js
@@ -105,7 +105,7 @@ app.get("/recommend/:keyword", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server is successfully running on port ${PORT}`);
 });
